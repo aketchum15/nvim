2688c45c9bf83704a3f67c30ee9759364a82d9f3
Adam Ketchum <ketchua@purdue.edu>
2025-04-15 14:35:42 -0400

    add lsp goto def/decl/ref

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,7 +1,10 @@
-
 vim.keymap.set('n', '<leader><space>', '<cmd>nohl<cr>', {})
 
 vim.keymap.set('n', '<C-n>', '<cmd>BufferNext<CR>', {})
 vim.keymap.set('n', '<C-p>', '<cmd>BufferPrevious<CR>', {})
 vim.keymap.set('n', '<C-x>', '<cmd>BufferDelete<CR>', {})
+
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
 
