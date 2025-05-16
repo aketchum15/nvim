0bfd53e3ab30c1d39e69988354b1c84ae0a3f9d0
Adam Ketchum <ketchua@purdue.edu>
2025-05-16 11:18:02 -0400

    dart tabbing

--- after/ftplugin/dart.lua
+++ after/ftplugin/dart.lua
@@ -1,2 +1,12 @@
+
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
+
+vim.keymap.set('n', '<leader>R', '<cmd>FlutterRestart<CR>', {})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*.dart",
+  callback = function()
+    vim.cmd("FlutterReload")
+  end,
+})
